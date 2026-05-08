0ec1df6d91584d9f310ef70db8f3c68fec2fe0ff
Erick Teixeira <erick.teixeira@fitenergia.com.br>
2026-05-07 23:51:43 -0300

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+DEPLOY_API=false
+DEPLOY_CLIENT=false
+
+for arg in "$@"; do
+  case $arg in
+    --api) DEPLOY_API=true ;;
+    --client) DEPLOY_CLIENT=true ;;
+  esac
+done
+
+if ! $DEPLOY_API && ! $DEPLOY_CLIENT; then
+  DEPLOY_API=true
+  DEPLOY_CLIENT=true
+fi
+
+if $DEPLOY_API; then
+  echo "==> Building & deploying API (Firebase Functions)..."
+  cd functions
+  firebase deploy --only functions
+  cd ..
+  echo "==> API deployed."
+fi
+
+if $DEPLOY_CLIENT; then
+  echo "==> Building & deploying client..."
+  cd client
+  npm run deploy
+  cd ..
+  echo "==> Client deployed."
+fi
+
+echo "==> Done."
